4d4dff85ba226d6d185b87b6da77db556301b30f
Ron Cemer <ron.cemer@lancium.com>
2025-06-27 14:47:10 -0400

    Fix indentation.

--- examples/rabbitmq/producer/main.go
+++ examples/rabbitmq/producer/main.go
@@ -6,24 +6,24 @@ import (
 
 	"github.com/go-kit/log"
 	celery "github.com/roncemer/gopher-celery-with-rabbitmq-broker"
-    celeryrabbitmq "github.com/roncemer/gopher-celery-with-rabbitmq-broker/rabbitmq"
+	celeryrabbitmq "github.com/roncemer/gopher-celery-with-rabbitmq-broker/rabbitmq"
 )
 
 func main() {
 	logger := log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
 
-    broker := celeryrabbitmq.NewBroker(celeryrabbitmq.WithAmqpUri("amqp://guest:guest@localhost:5672/"))
+	broker := celeryrabbitmq.NewBroker(celeryrabbitmq.WithAmqpUri("amqp://guest:guest@localhost:5672/"))
 	app := celery.NewApp(
-        celery.WithBroker(broker),
+	    celery.WithBroker(broker),
 		celery.WithLogger(logger),
 		celery.WithTaskProtocol(2),
 	)
 	err := app.Delay("myproject.mytask", "important", "fizz", "bazz")
 	logger.Log("msg", "task was sent using protocol v2", "err", err)
 
-    broker = celeryrabbitmq.NewBroker(celeryrabbitmq.WithAmqpUri("amqp://guest:guest@localhost:5672/"))
+	broker = celeryrabbitmq.NewBroker(celeryrabbitmq.WithAmqpUri("amqp://guest:guest@localhost:5672/"))
 	app = celery.NewApp(
-        celery.WithBroker(broker),
+	    celery.WithBroker(broker),
 		celery.WithLogger(logger),
 		celery.WithTaskProtocol(1),
 	)
